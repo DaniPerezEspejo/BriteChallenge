ce6ccec6fe2045131c7973162b09871c793021b9
Daniel Perez <danielperezespejo@gmail.com>
2025-04-16 19:54:04 +0200

    feat: api tests

---
+++ cypress/e2e/api/pokeapi-tests.cy.js
@@ -0,0 +1,75 @@
+/// <reference types="cypress" />
+
+import { onPokeApi } from "../../endpoints/pokeapi";
+
+describe('Brite Challenge - Pokeapi testing', () => {
+
+    it('Request Berry endpoint - Request by Id - Valid Id', () => {
+        const backendBaseUrl = Cypress.env('baseServerUrl');
+
+        cy.fixture('public-endpoints').then(data => {
+            const berryEndpointData = data.find(d => d.alias === 'getBerry')
+            const request = backendBaseUrl + berryEndpointData.endpoint;
+
+            onPokeApi.checkApiBerryEndpoint(request, '1', 200);
+        })
+    })
+
+    it('Request Berry endpoint - Request by Id - Invalid Id', () => {
+        const backendBaseUrl = Cypress.env('baseServerUrl');
+
+        cy.fixture('public-endpoints').then(data => {
+            const berryEndpointData = data.find(d => d.alias === 'getBerry')
+            const request = backendBaseUrl + berryEndpointData.endpoint;
+
+            onPokeApi.checkApiBerryEndpoint(request, 'invalidId', 404);
+        })
+    })
+
+    it('Request Berry endpoint - Request by Name - Valid Name', () => {
+        const backendBaseUrl = Cypress.env('baseServerUrl');
+
+        cy.fixture('public-endpoints').then(data => {
+            const berryEndpointData = data.find(d => d.alias === 'getBerry')
+            const request = backendBaseUrl + berryEndpointData.endpoint;
+
+            onPokeApi.checkApiBerryEndpoint(request, 'cheri', 200);
+        })
+    })
+
+    it('Request Berry endpoint - Request by name - Invalid name', () => {
+        const backendBaseUrl = Cypress.env('baseServerUrl');
+
+        cy.fixture('public-endpoints').then(data => {
+            const berryEndpointData = data.find(d => d.alias === 'getBerry')
+            const request = backendBaseUrl + berryEndpointData.endpoint;
+
+            onPokeApi.checkApiBerryEndpoint(request, 'invalidName', 404);
+        })
+    })
+
+    it('Request Berry Flavor endpoint - Request spicy ones - Get most potent one - Get berry details ', () => {
+        const backendBaseUrl = Cypress.env('baseServerUrl');
+
+        cy.fixture('public-endpoints').then(data => {
+            const berryEndpointData = data.find(d => d.alias === 'getBerry')
+            const berryFlavorEndpointData = data.find(d => d.alias === 'getBerryFlavor')
+
+            const requestBerry = backendBaseUrl + berryEndpointData.endpoint;
+            const requestBerryFlavor = backendBaseUrl + berryFlavorEndpointData.endpoint;
+
+            cy.request(requestBerryFlavor + 'spicy').then((response) => {
+                expect(response.status).to.eq(200);
+
+                const berries = response.body.berries;
+                expect(berries).to.have.length.greaterThan(0)
+
+                const mostPotentBerry = berries.reduce((max, current) => {
+                    return current.potency > max.potency ? current : max
+                })
+            
+                onPokeApi.checkApiBerryEndpoint(requestBerry, mostPotentBerry.berry.name, 200);
+            })
+        })
+    })
+})
---
+++ cypress/endpoints/pokeApi.js
@@ -0,0 +1,56 @@
+export class PokeApi {
+
+    /** Checks getBerry endpoint and verifies response schema
+     * @param {string} endpoint Base endpoint to request, with no query params
+     * @param {string} queryParams Additional endpoints params (id or name in this case)
+     * @param {number} status Expected status
+     */    
+    checkApiBerryEndpoint(endpoint, queryParams, status) {
+        cy.request({
+                url: endpoint + queryParams,
+                failOnStatusCode: false
+        }).then((response) => {
+            if(status === 200)
+            {
+                expect(response.status).to.eq(200);
+                this.checkBerrySchema(response.body);
+            }
+
+            if(status === 404)
+            {
+                expect(response.status).to.eq(404);
+            }
+        })
+    }
+
+    // Helper function to verify Berry schema
+    checkBerrySchema(body) {
+        expect(body).to.have.property('id').and.to.be.a('number');
+        expect(body).to.have.property('name').and.to.be.a('string');
+        expect(body).to.have.property('growth_time').and.to.be.a('number');
+        expect(body).to.have.property('size').and.to.be.a('number');
+        expect(body).to.have.property('smoothness').and.to.be.a('number');
+        expect(body).to.have.property('soil_dryness').and.to.be.a('number');
+
+        expect(body).to.have.property('firmness');
+        expect(body.firmness).to.have.property('name').and.to.be.a('string');
+        expect(body.firmness).to.have.property('url').and.to.be.a('string');
+
+        expect(body).to.have.property('flavors').and.to.be.an('array');
+
+        if (body.flavors.length > 0) {
+            const flavor = body.flavors[0];
+
+            expect(flavor).to.have.property('potency').and.to.be.a('number');
+            expect(flavor).to.have.property('flavor');
+            expect(flavor.flavor).to.have.property('name').and.to.be.a('string');
+            expect(flavor.flavor).to.have.property('url').and.to.be.a('string');
+        }
+
+        expect(body).to.have.property('item');
+        expect(body.item).to.have.property('name').and.to.be.a('string');
+        expect(body.item).to.have.property('url').and.to.be.a('string');
+    }
+}
+
+export const onPokeApi = new PokeApi();
